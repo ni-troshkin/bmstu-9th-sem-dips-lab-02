a0dd792ee87452f8a5595ffb12242e5e5f9e8fd8
Nickolay Troshkin <nicktrs@mail.ru>
2023-10-25 23:36:56 +0300

    Добавить слой репозитория в сервис проката книг

---
+++ app/server/services/reservations/ReservationService/src/main/java/com/reservationservice/entity/Reservation.java
@@ -0,0 +1,22 @@
+package com.reservationservice.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Builder
+@Getter
+@AllArgsConstructor
+public class Reservation {
+    int id;
+    UUID reservationUid;
+    String username;
+    UUID bookUid;
+    UUID libraryUid;
+    Status status;
+    LocalDateTime startDate;
+    LocalDateTime tillDate;
+}
